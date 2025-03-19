d7875ad0b96cb1a06dcf88a47487c0f5a702ce53
Nandhini299 <nandhinni14082005@gmail.com>
2025-03-19 23:57:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+// var, let, const three types of variables
+var on_btn = document.getElementById("on");
+var off_btn = document.getElementById("off");
+var img = document.getElementById("bulb");
+
+function bulb_on()
+{
+    img.src="https://cdn.pixabay.com/photo/2013/07/12/14/34/light-148483_1280.png";
+}
+
+function bulb_off()
+{
+    img.src=" https://cdn.pixabay.com/photo/2013/07/12/14/34/filament-148482_1280.png";
+}
